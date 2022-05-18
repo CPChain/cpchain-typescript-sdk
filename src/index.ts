e2cf8d72b4ab86dfe9624d6af7eeccab1052b270
zgljl2012 <2693491512@qq.com>
2022-05-18 23:16:46 +0800

    change to es6 and export BigNumber

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import * as providers from './providers'
 import wallets from './wallets'
 import utils from './utils'
 import contract from './contract'
+export { BigNumber } from 'ethers'
+export { type CPCWallet } from './wallets'
+export { type CPCJsonRpcProvider } from './providers'
 
 export default {
   providers,
