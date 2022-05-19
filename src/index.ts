86484a81d860bc3f2c1044acf89c15c860e5f0ac
zgljl2012 <2693491512@qq.com>
2022-05-19 14:01:10 +0800

    upgrade to 0.0.19

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,36 @@ import * as providers from './providers'
 import wallets from './wallets'
 import utils from './utils'
 import contract from './contract'
+import { BigNumber } from 'ethers'
 export { BigNumber } from 'ethers'
 export { CPCWallet } from './wallets'
 export { CPCJsonRpcProvider } from './providers'
 export { Contract, ContractFactory } from './contract'
+export { uint256, bool, address, uint64, uint8, int8 } from './types'
+
+export interface Receipt {
+  to: string
+  from: string
+  gasUsed: BigNumber
+  contractAddress: string | null
+  transactionIndex: number
+  logsBloom: string
+  blockHash: string
+  transactionHash: string
+  blockNumber: number
+  confirmations: number
+  cumulativeGasUsed: BigNumber
+  status: number // 1 success, 0 failed
+  type: number
+  byzantium: boolean
+  logs: any[]
+  events: any[]
+}
+
+export interface TxResult{
+  hash: string
+  wait(): Promise<Receipt>
+}
 
 export default {
   providers,
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+import { BigNumber } from 'ethers'
+
+export type uint256 = BigNumber
+export type bool = boolean
+export type address = string
+export type uint64 = number
+export type uint8 = number
+export type int8 = number
