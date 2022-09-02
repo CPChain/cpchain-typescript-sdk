d7b2c39b52e05af25a5228f39359df571dd5ee80
zgljl2012 <2693491512@qq.com>
2022-09-02 21:53:28 +0800

    fix bug

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,8 +1,32 @@
-import { isNode, isDeno, isJsDom } from 'browser-or-node'
 import crossFetch from 'cross-fetch'
 
+const isBrowser =
+  typeof window !== 'undefined' && typeof window.document !== 'undefined'
+
+const isNode =
+  typeof process !== 'undefined' &&
+  process.versions != null &&
+  process.versions.node != null
+
+const isWebWorker =
+  typeof self === 'object' &&
+  self.constructor &&
+  self.constructor.name === 'DedicatedWorkerGlobalScope'
+
+/**
+ * @see https://github.com/jsdom/jsdom/releases/tag/12.0.0
+ * @see https://github.com/jsdom/jsdom/issues/1537
+ */
+const isJsDom =
+  (typeof window !== 'undefined' && window.name === 'nodejs') ||
+  (typeof navigator !== 'undefined' && navigator.userAgent && Array.isArray(navigator.userAgent) &&
+    (navigator.userAgent.includes('Node.js') ||
+      navigator.userAgent.includes('jsdom')))
+
+export { isBrowser, isWebWorker, isNode, isJsDom }
+
 export const xfetch = (() => {
-  if (isNode || isDeno || isJsDom) {
+  if (isNode || isJsDom) {
     return crossFetch
   }
   return fetch
